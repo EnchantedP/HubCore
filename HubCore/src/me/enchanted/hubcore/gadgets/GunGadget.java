212e50b01830c3bc93ec3ec26dec2475cb6594be
busbydagen <busbydagen@Art-10.BECKETS.LOCAL>
2018-11-06 08:55:47 +0000

    gun

---
+++ HubCore/src/me/enchanted/hubcore/gadgets/GunGadget.java
@@ -0,0 +1,28 @@
+package me.enchanted.hubcore.gadgets;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.Vector;
+
+public class GunGadget implements Listener{
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onShoot(PlayerInteractEvent e)
+	{
+		Player p = e.getPlayer();
+		if(p.getItemInHand().getType() == Material.GOLD_BARDING)
+		{
+			Snowball sb = p.getWorld().spawn(p.getLocation(), Snowball.class);
+			
+			Vector v = p.getLocation().getDirection();
+			sb.setVelocity(v.multiply(1.7));
+			
+		}
+	}
+
+}
